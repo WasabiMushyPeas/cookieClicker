1cd4ef027bc5b1c4141b566cd2a947cddfaca595
Jackson <54775398+WasabiMushyPeas@users.noreply.github.com>
2023-09-21 15:51:25 -0600

    working simple odometer

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
 let cookies = 0;
 let grandmas = 0;
 
-function bakeCookie() {
-    cookies += 1;
-    document.getElementById("cookies").innerHTML = cookies;
-}
-
-function buyGrandma() {
-    grandmas += 1;
-    cookies -= 100;
-    document.getElementById("grandmas").innerHTML = grandmas;
-    document.getElementById("cookies").innerHTML = cookies;
-    grandmaBakeCookie();
-}
-
-function grandmaBakeCookie(){
-    while(true){
-    cookies += grandmas;
-    document.getElementById("cookies").innerHTML = cookies;
-    setTimeout(grandmaBakeCookie, 1);
-    grandmaBakeCookie();
-    }
-
-}
-
 
 
 // Number to odometer
@@ -32,52 +9,56 @@ function numberToOdometer(number) {
     let digits = number.toString().length;
     //create an array of digits
     let digitArray = number.toString().split("");
+
+
+    //log
+    console.log("Number: " + number);
+    console.log("Digits: " + digits);
+    console.log("Digit Array: " + digitArray);
+
     //map the amount of digits to the amount of odometer digits hide the rest
     let odometerDigits = document.getElementsByClassName("zerosPlaceHolders");
     for (let i = 0; i < odometerDigits.length; i++) {
         if (i < digits) {
             odometerDigits[i].style.display = "block";
+            console.log("Displaying digit " + i);
         } else {
             odometerDigits[i].style.display = "none";
+            console.log("Hiding digit " + i);
         }
     }
 
     //translateY the odometer digits to the number
-    for (let i = 0; i < digitArray.length; i++){
-        //Switch case statement to translate the digits to the correct number
-        switch (i) {
-            case 1:
-                odometerDigits[i].style.transform = "translateY(0px)";
-                break;
-            case 2:
-                odometerDigits[i].style.transform = "translateY(-100px)";
-                break;
-            case 3:
-                odometerDigits[i].style.transform = "translateY(-200px)";
-                break;
-            case 4:
-                odometerDigits[i].style.transform = "translateY(-300px)";
-                break;
-            case 5:
-                odometerDigits[i].style.transform = "translateY(-400px)";
-                break;
-            case 6:
-                odometerDigits[i].style.transform = "translateY(-500px)";
-                break;
-            case 7:
-                odometerDigits[i].style.transform = "translateY(-600px)";
-                break;
-            case 8:
-                odometerDigits[i].style.transform = "translateY(-700px)";
-                break;
-            case 9:
-                odometerDigits[i].style.transform = "translateY(-800px)";
-                break;
-            case 10:
-                odometerDigits[i].style.transform = "translateY(-900px)";
-                break;
-        }
+    for (let i = 0; i < digitArray.length+1; i++){
+        console.log("Setting " + digitArray[i] + " to " + i);
+        odometerDigits[i].style.transform = "translateY(" + digitToPosition(parseInt(digitArray[i])+1) + "vw)";
     }
 }
 
-numberToOdometer(12345);
+let zeroOffset = 26.8;
+let numberSpacing = 6;
+
+function digitToPosition(number){
+    return zeroOffset - (numberSpacing * number);
+}
+
+// function positionToDigit(position){
+//     return (zeroOffset - position) / numberSpacing;
+// }
+
+// // Function that linearly animates from one digitToPosition to another and returns the value
+// function animateDigitToPosition(start, end, duration) {
+//     let difference = end - start;
+//     let startTime = new Date().getTime();
+//     let endTime = startTime + duration;
+//     let current = start;
+//     let step = function () {
+//         let now = new Date().getTime();
+//         let remaining = Math.max((endTime - now) / duration, 0);
+//         current = end - (remaining * difference);
+//         return current;
+//     };
+//     return step();
+// }
+
+numberToOdometer(10231);
