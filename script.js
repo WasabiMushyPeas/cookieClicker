f5118d78d2097f65e76791e5967373d61e25ee77
WasabiMushyPeas <Jackson.McDowell@gmail.com>
2023-10-10 16:17:53 -0600

    cookies not working?

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ let gameLoop = 2;
 let transitionTime = 0.5;
 
 
-function startGame(){
+function startGame() {
     loadGame();
     numberToOdometer(cookies);
     grandmaLoop();
+    cookiesPerSecondLoop();
 }
 
 
@@ -22,30 +23,32 @@ function cookieClick() {
     cookies++;
     totalCookies++;
     numberToOdometer(cookies);
-    saveGame();
 }
 
 // Buy Grandma Button
-function buyGrandma(){
-    if (cookies >= grandmaCost){
+function buyGrandma() {
+    if (cookies >= grandmaCost) {
         // increase the cost of the next grandma by ((1/10000)x^e)+10
-        grandmaCost = Math.floor((1/500 * Math.pow(grandmas, 2.718281828459045)) + 10);
+        grandmaCost = Math.floor((1 / 500 * Math.pow(grandmas, 2.718281828459045)) + 10);
         numberToOdometer(cookies);
         grandmas++;
         cookies -= grandmaCost;
         updateGrandmas();
     }
-    saveGame();
     updateCookiesPerSecond();
 }
 
 
 // Grandma Loop 
-async function grandmaLoop(){
-    if(grandmas>0){
+async function grandmaLoop() {
+    if (grandmas > 0 && grandmas <= 50) {
         cookieClick();
     }
-    setTimeout(grandmaLoop, (1000/(grandmas+1)));
+    if (grandmas > 50) {
+        cookies += (grandmas / 2) - 1;
+        cookieClick();
+    }
+    setTimeout(grandmaLoop, (1000 / (grandmas + 1)));
 }
 
 
@@ -77,13 +80,13 @@ function numberToOdometer(number) {
     }
 
     //translateY the odometer digits to the number
-    for (let i = 0; i < digitArray.length+1; i++){
+    for (let i = 0; i < digitArray.length + 1; i++) {
         // console.log("Setting " + digitArray[i] + " to " + i);
 
-        odometerDigits[i].style.transform = "translateY(" + digitToPosition(parseInt(digitArray[i])+1) + "vw)";
+        odometerDigits[i].style.transform = "translateY(" + digitToPosition(parseInt(digitArray[i]) + 1) + "vw)";
 
         // Based on how fast the digit is moving, change the transition time 
-        let tempTransitionTime = transitionTime - ((transitionTime * (i/(digitArray.length))));
+        let tempTransitionTime = transitionTime - ((transitionTime * (i / (digitArray.length))));
         // if(i == digitArray.length && cookiesPerSecond > 15) {
         //     tempTransitionTime = 0;
         // }
@@ -92,14 +95,14 @@ function numberToOdometer(number) {
 
         // Get the all the p child of the odometer digit and set all of them to transparent except the one that is the number that is in focus
         let pChildren = odometerDigits[i].getElementsByTagName("p");
-        for (let j = 0; j < pChildren.length; j++){
-            if (j == parseInt(digitArray[i])){
+        for (let j = 0; j < pChildren.length; j++) {
+            if (j == parseInt(digitArray[i])) {
                 pChildren[j].style.opacity = "1";
             } else {
                 pChildren[j].style.opacity = "0.2";
             }
         }
-        
+
     }
     updateTotalCookies();
 }
@@ -109,58 +112,63 @@ let numberSpacing = 6;
 
 
 //translate the number to the position on the odometer
-function digitToPosition(number){
+function digitToPosition(number) {
     return zeroOffset - (numberSpacing * number);
 }
 
 // Update the number of cookies per second
-function updateCookiesPerSecond(){
-    cookiesPerSecond = grandmas*gameLoop;
+function updateCookiesPerSecond() {
     document.getElementById("amountOfCookiesPerSecond").innerHTML = cookiesPerSecond.toString() + " c/sec";
     document.getElementById("amountOfCookiesPerSecond").style.fontSize = "2vw";
 }
 
 // Update the number of grandmas
-function updateGrandmas(){
+function updateGrandmas() {
     document.getElementById("amountOfGrandmas").innerHTML = "Grandmas: " + grandmas.toString();
     document.getElementById("amountOfGrandmas").style.fontSize = "2vw";
-    document.getElementById("grandmaCost").innerHTML = "Buy Grandma ¢" + (Math.floor((1/500 * Math.pow(grandmas, 2.718281828459045)) + 10)).toString();
+    document.getElementById("grandmaCost").innerHTML = "Buy Grandma ¢" + (Math.floor((1 / 500 * Math.pow(grandmas, 2.718281828459045)) + 10)).toString();
     document.getElementById("grandmaCost").style.fontSize = "2vw";
 }
 
 //Update the number of total cookies
-function updateTotalCookies(){
+function updateTotalCookies() {
     document.getElementById("amountOfCookies").innerHTML = "Total Cookies: " + totalCookies.toString();
     document.getElementById("amountOfCookies").style.fontSize = "2vw";
 }
 
 // Save the game in cookies that expire in 5 years
-function saveGame(){
+function saveGame() {
     let d = new Date();
-    d.setTime(d.getTime() + (5*365*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
+    d.setTime(d.getTime() + (5 * 365 * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    console.log(expires);
     document.cookie = "cookies=" + cookies.toString() + ";" + expires + ";path=/";
+    console.log("cookies=" + cookies.toString() + ";" + expires + ";path=/");
     document.cookie = "grandmas=" + grandmas.toString() + ";" + expires + ";path=/";
+    console.log("grandmas=" + grandmas.toString() + ";" + expires + ";path=/");
     document.cookie = "totalCookies=" + totalCookies.toString() + ";" + expires + ";path=/";
+    console.log("totalCookies=" + totalCookies.toString() + ";" + expires + ";path=/");
+    console.log("Saved game");
+    console.log(document.cookie);
 }
 
 // Load the game from cookies
-function loadGame(){
+function loadGame() {
     let cookieArray = document.cookie.split(";");
-    for (let i = 0; i < cookieArray.length; i++){
+    for (let i = 0; i < cookieArray.length; i++) {
         let cookie = cookieArray[i].split("=");
         //console.log(cookie);
-        if (cookie[0] == " cookies"){
+        if (cookie[0] == " cookies") {
             cookies = parseInt(cookie[1]);
             console.log(cookies);
-        } else if (cookie[0] == " grandmas"){
+        } else if (cookie[0] == " grandmas") {
             grandmas = parseInt(cookie[1]);
             console.log(grandmas);
-        } else if (cookie[0] == " totalCookies"){
+        } else if (cookie[0] == " totalCookies") {
             totalCookies = parseInt(cookie[1]);
             console.log(totalCookies);
         }
-        console.log(cookie[0] + " " + " i:" + i);
+        // console.log(cookie[0] + " " + " i:" + i);
     }
 
     updateCookiesPerSecond();
@@ -169,11 +177,13 @@ function loadGame(){
     updateGrandmas();
 }
 
-
-
-window.onbeforeunload = closingCode;
-
-function closingCode(){
-    saveGame();
-    return null;
+// function that looks at the difference between cookies 1 second ago and now and calculates the cookies per second that runs in the background
+function cookiesPerSecondLoop() {
+    let cookiesPerSecondTemp = cookies;
+    setTimeout(function () {
+        cookiesPerSecond = cookies - cookiesPerSecondTemp;
+        updateCookiesPerSecond();
+        saveGame();
+        cookiesPerSecondLoop();
+    }, 1000);
 }
